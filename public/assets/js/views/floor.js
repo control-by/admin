66504a43c6d2ff0ab6cea6bae16b15e43b3cae58
Piotruś <piotr@webkameleon.com>
2016-06-18 06:00:52 +0000

    err

--- public/assets/js/views/floor.js
+++ public/assets/js/views/floor.js
@@ -613,7 +613,7 @@ var floorDrawElements=function(data) {
     for(var i=0;i<data.length;i++) {
         var matchFound=false;
         
-        if (data[i].controls!==undefined && data[i].control!=null) {
+        if (data[i].controls!==undefined && data[i].controls!=null) {
             for (var j=0; j<data[i].controls.length; j++) {
                 if (data[i].controls[j].haddr !== undefined) {
                     haddrs.push(data[i].controls[j].haddr);
