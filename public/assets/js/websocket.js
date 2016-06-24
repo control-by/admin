4a556a392e091f64821d0693e1eaf78393d1bff1
Piotr Podstawski <piotr@webkameleon.com>
2016-06-24 21:14:42 +0200

    iframe

--- public/assets/js/websocket.js
+++ public/assets/js/websocket.js
@@ -148,7 +148,7 @@ websocket.on('web',function(web) {
     if (host.indexOf('webkameleon')>=0) {
         for (i=0;i<web.ips.length;i++) {
             var url='http://'+web.ips[i]+':'+web.port;
-            $.get(url+'/check-homiq-web',function (data) {
+            $.get(url+'/check-web',function (data) {
                 if (data=='OK') {
                     $('body').html('<iframe src="'+url+'"></iframe>');
                 }
